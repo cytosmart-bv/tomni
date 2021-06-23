715b9c22081184847d3450b05b7aac5efe578ceb
wagenrace <nijhof.dns@gmail.com>
2021-06-23 11:19:03 +0200

    Bump version: 1.7.2 → 1.7.3

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/cytosmart-bv/tomni",
-    version="1.7.2",
+    version="1.7.3",
     zip_safe=False,
 )
