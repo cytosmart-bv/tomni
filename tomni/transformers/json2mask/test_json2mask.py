da3cb230ef67d6856a3219306130a39642103b06
wagenrace <nijhof.dns@gmail.com>
2021-10-27 09:39:34 +0200

    test json2mask with floats and big ellipsis
    Signed-off-by: wagenrace <nijhof.dns@gmail.com>

--- tomni/transformers/json2mask/test_json2mask.py
+++ tomni/transformers/json2mask/test_json2mask.py
@@ -223,6 +223,106 @@ class TestJson2Mask(TestCase):
 
         assert_array_equal(expected_result, result)
 
+    def test_circle_floats(self):
+        size = (11, 13)
+        json_objects = [
+            {
+                "type": "ellipse",
+                "center": {"x": 6., "y": 4.},
+                "radiusX": 6.,
+                "radiusY": 4.,
+                "angleOfRotation": 0,
+                "id": "unicorn",
+            }
+        ]
+
+        expected = np.array(
+            [
+                [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+                [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ]
+        )
+
+        result = json2mask(json_objects, size, 0)
+        assert_array_equal(result, expected)
+
+    def test_big_circle(self):
+        size = (10000, 10001)
+
+        json_objects = [
+            {
+                "type": "ellipse",
+                "center": {"x": 5000, "y": 2467},
+                "radiusX": 3200,
+                "radiusY": 1692,
+                "angleOfRotation": 0,
+                "id": "unicorn",
+            }
+        ]    
+
+        mask = json2mask(json_objects, size, 0)
+        self.assertEqual(True, mask[2467][5000])
+        self.assertEqual(True, mask[3000][5000])
+
+        # X minimum
+        self.assertEqual(False, mask[2467][1798])
+        self.assertEqual(True, mask[2467][1801])
+
+        # X Maximum
+        self.assertEqual(True, mask[2467][8198])
+        self.assertEqual(False, mask[2467][8201])
+        
+        # Y minimum
+        self.assertEqual(False, mask[773][5000])
+        self.assertEqual(True, mask[777][5000])
+
+        # Y maximum
+        self.assertEqual(True, mask[4157][5000])
+        self.assertEqual(False, mask[4161][5000])
+
+    def test_big_circle_floats(self):
+        size = (10000, 10001)
+
+        json_objects = [
+            {
+                "type": "ellipse",
+                "center": {"x": 5000., "y": 2467.},
+                "radiusX": 3200.,
+                "radiusY": 1692.,
+                "angleOfRotation": 0,
+                "id": "unicorn",
+            }
+        ]    
+
+        mask = json2mask(json_objects, size, 0)
+        self.assertEqual(True, mask[2467][5000])
+        self.assertEqual(True, mask[3000][5000])
+
+        # X minimum
+        self.assertEqual(False, mask[2467][1798])
+        self.assertEqual(True, mask[2467][1801])
+
+        # X Maximum
+        self.assertEqual(True, mask[2467][8198])
+        self.assertEqual(False, mask[2467][8201])
+        
+        # Y minimum
+        self.assertEqual(False, mask[773][5000])
+        self.assertEqual(True, mask[777][5000])
+
+        # Y maximum
+        self.assertEqual(True, mask[4157][5000])
+        self.assertEqual(False, mask[4161][5000])
+
     def test_circle(self):
         size = (11, 13)
         json_objects = [
@@ -277,7 +377,7 @@ class TestJson2Mask(TestCase):
         ]
 
         expected = np.zeros((500, 500), dtype=np.uint8)
-        expected[75:125, 75:125] = make_mask_circle((50, 50), 50)
+        expected[75:126, 75:126] = make_mask_circle((51, 51), 50)
 
         result = json2mask(json_objects, size, 0)
         assert_array_equal(result, expected)
