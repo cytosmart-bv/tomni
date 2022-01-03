b5334f741ffe698f54acd05fd73fa626c08f42ab
wagenrace <nijhof.dns@gmail.com>
2022-01-03 15:40:19 +0100

    Bump version: 1.9.1 → 1.9.2

--- tomni/__init__.py
+++ tomni/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = """Tom Nijhof & Jelle van Kerkvoorde"""
 __email__ = "tom.nijhof@cytosmart.com"
-__version__ = "1.9.1"
+__version__ = "1.9.2"
 
 from .bbox_fitting import bbox_fitting, bbox_fitting_center
 from .img_paste import img_paste
