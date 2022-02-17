005590d0774a243f28b87f92a58b88c47e5606fb
wagenrace <nijhof.dns@gmail.com>
2022-02-17 15:26:50 +0100

    add approximate circle from contour
    Signed-off-by: wagenrace <nijhof.dns@gmail.com>

---
+++ tomni/contour_operations/approximate_circle_by_area/test_get_approx_circle_by_area.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from .main import approximate_circle_by_area
+import numpy as np
+
+class TestGetAppoxCircleArea(TestCase):
+    def test_free_shape(self):
+        contour = np.array([[[70, 120]], [[80, 80]], [[70, 40]], [[60, 80]]])
+
+        expected_result = (70, 80, 15.9576912161)
+        result = approximate_circle_by_area(contour)
+        print(result)
+
+        self.assertEqual(len(result), len(expected_result))
+        for i in range(len(result)):
+            self.assertAlmostEqual(result[i], expected_result[i], 4)
