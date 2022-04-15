747c1826412a0f0820433caeeff5e12e5a772b29
wagenrace <nijhof.dns@gmail.com>
2022-04-15 10:12:58 +0200

    fix docstrings
    Signed-off-by: wagenrace <nijhof.dns@gmail.com>

--- tomni/contour_operations/approximate_circle_by_area/main.py
+++ tomni/contour_operations/approximate_circle_by_area/main.py
@@ -9,8 +9,7 @@ from .. import get_center
 
 def approximate_circle_by_area(contour: np.ndarray) -> Tuple[float, float, float]:
     """
-    approximate a circle with the same area as the contour
-    The center 
+    Return a circle with the same area and center as the contour.
 
     Args:
         contour (np.ndarray): An opencv contour of a single object
@@ -19,8 +18,8 @@ def approximate_circle_by_area(contour: np.ndarray) -> Tuple[float, float, float
         x: (float) Center x postion
         y: (float) Center y postion
         radius: (float) Radius of the circle
-    """    
+    """
     x, y = get_center(contour)
     area = cv2.contourArea(contour)
-    radius = sqrt(area/pi)
+    radius = sqrt(area / pi)
     return x, y, radius
--- tomni/contour_operations/get_center/main.py
+++ tomni/contour_operations/get_center/main.py
@@ -1,13 +1,21 @@
 from typing import Tuple
-from typing import Tuple
 
 import cv2
 import numpy as np
 
+
 def get_center(contour: np.ndarray) -> Tuple[int, int]:
-    '''
+    """
     Return the center of a contour
-    '''
+
+    Args:
+        contour (np.ndarray): An opencv contour of a single object
+
+    Returns:
+        x: (float) Center x postion
+        y: (float) Center y postion
+
+    """
     M = cv2.moments(contour)
     if M["m00"] == 0:
         return contour[0][0][0], contour[0][0][1]
