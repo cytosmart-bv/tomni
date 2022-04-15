e1379715ae64bd65c0bd7899d5470046ca1139a4
wagenrace <nijhof.dns@gmail.com>
2022-04-15 13:47:26 +0200

    add roundness
    Signed-off-by: wagenrace <nijhof.dns@gmail.com>

--- HISTORY.rst
+++ HISTORY.rst
@@ -3,6 +3,7 @@
 1.12.0 (2022-04-15)
 ------------------
 - BUGFIX: Import approximate_circle_by_area
+- Add roundness
 
 1.11.0 (2022-04-11)
 ------------------
---
+++ tomni/contour_operations/roundness/main.py
@@ -0,0 +1,24 @@
+from typing import Tuple
+
+import cv2
+import numpy as np
+
+
+def roundness(contour: np.ndarray) -> float:
+    """
+    Return roundess of the contour
+    Roundness is terminend by difference in areas of the contour and enclosing circle
+
+
+    Args:
+        contour (np.ndarray): An opencv contour of a single object
+
+    Returns:
+        float: roundess number between 0 and 1
+    """
+
+    area = cv2.contourArea(contour)
+    _, radius = cv2.minEnclosingCircle(contour)
+    enclosing_circle_area = radius**2 * np.pi
+    roundness = area / enclosing_circle_area
+    return roundness
