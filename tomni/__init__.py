323379d21d812b44b79df3103517be1065f67f8a
LAPTOP-KV55JQ73\janni <janniklas.schneider@cytosmart.com>
2022-07-04 14:35:14 +0200

    Update inits. Add test.

--- tomni/__init__.py
+++ tomni/__init__.py
@@ -5,13 +5,15 @@ __author__ = """Tom Nijhof & Jelle van Kerkvoorde"""
 __email__ = "tom.nijhof@cytosmart.com"
 __version__ = "1.12.0"
 
+from . import (
+    bbox_operations,
+    illumination_correction,
+    json_operations,
+    make_mask,
+    transformers,
+)
 from .bbox_fitting import bbox_fitting, bbox_fitting_center
+from .img_center_crop import crop_by_dim, crop_by_scale
+from .img_dim import img_dim
 from .img_paste import img_paste
 from .shape_fitting import fit_rect_around_ellipse
-from .img_dim import img_dim
-
-from . import illumination_correction
-from . import make_mask
-from . import transformers
-from . import json_operations
-from . import bbox_operations
