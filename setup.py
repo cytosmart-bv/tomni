e9b57b16b91a208dc7d4e2aef1c7e6ee69a1b19a
wagenrace <nijhof.dns@gmail.com>
2022-11-18 16:17:46 +0100

    make v2 beta
    Signed-off-by: wagenrace <nijhof.dns@gmail.com>

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/cytosmart-bv/tomni",
-    version="1.13.0",
+    version="2.0.0b0",
     zip_safe=False,
 )
