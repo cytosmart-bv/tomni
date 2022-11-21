68f0b8f4b480b665a94a58ae25c3aea8fcf5b98a
LAPTOP-KV55JQ73\janni <janniklas.schneider@cytosmart.com>
2022-11-21 15:31:33 +0100

    Bump version: 2.0.0 → 2.0.0b0

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/cytosmart-bv/tomni",
-    version="2.0.0",
+    version="2.0.0b0",
     zip_safe=False,
 )
