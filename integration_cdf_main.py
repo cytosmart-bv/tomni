76f092b6518ebadf9a9fe3860fc3f1fbf4707cc6
LAPTOP-KV55JQ73\janni <janniklas.schneider@cytosmart.com>
2022-11-22 10:38:50 +0100

    Update integration to use filedialog.

--- integration_cdf_main.py
+++ integration_cdf_main.py
@@ -5,13 +5,7 @@ from tkinter import filedialog
 from tomni.cytosmart_data_format import CytoSmartDataFormat
 
 #%%
-# json_filepaths = filedialog.askopenfilename(title="Select CDF JSONs.")
-
-# json_fp = r"C:\Users\janni\Data\julia_thp_exp4\6eccfc85-b5b6-4eae-93dd-1af4690dc8bc_brightfield_1644225105198.json"
-# json_fp = r"C:\Users\janni\Downloads\5ccaf1a7_brightfield_1612861524296_v7_scf.json"
-json_fp = r"C:\Users\janni\Data\ellipse.json"
-
-# TODO: do ellipse
+json_fp = filedialog.askopenfilename(title="Select CDF JSONs.")
 
 with open(json_fp, "rb") as f:
     dicts = json.load(f)
@@ -22,11 +16,14 @@ print(f"__len__: {len(cdf)}")
 
 #%%
 # cdf creates a generator.
+count = 0
 for annotation in cdf:
-    print()
+    count += 1
+print(f"Count: {count}")
+
 
 #%%
-dicts_ = cdf.to_cdf()
+dicts_ = cdf.to_dict()
 with open("temp.json", "w") as f:
     json.dump(dicts_, f)
 
