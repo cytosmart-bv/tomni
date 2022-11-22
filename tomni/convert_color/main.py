2e0aa3e517e3577d7f53a7be90718e7e6771fd77
wagenrace <nijhof.dns@gmail.com>
2022-11-22 12:55:25 +0100

    change type to color
    Signed-off-by: wagenrace <nijhof.dns@gmail.com>

--- tomni/convert_color/main.py
+++ tomni/convert_color/main.py
@@ -3,17 +3,17 @@ import numpy as np
 import cv2
 
 
-def convert_color(img: np.ndarray, new_type: Union[str, int]) -> np.ndarray:
+def convert_color(img: np.ndarray, new_color: Union[str, int]) -> np.ndarray:
     """
-    Convert a the color of an image to a wanted type, without needed to know the current type.
+    Convert a the color of an image to a wanted color, without needed to know the current color.
     It will assume only gray, BGR, and BGRA exists (these are the common openCV color types)
 
     Args:
         img (np.ndarray uint8): The image that needs to be converted. This image is gray, BGR or BGRA
-        newType (Union[str, int]): Type the new image should be gray (1), BGR (2) or BGRA (3)
+        new_color (Union[str, int]): Color the new image should be gray (1), BGR (2) or BGRA (3)
 
     Raises:
-        ValueError: newType is not supported
+        ValueError: new_color is not supported
         ValueError: img has more then 3 dimensions or less then 2
         ValueError: img as a number of channels that is not 1, 3, or 4
 
@@ -22,21 +22,21 @@ def convert_color(img: np.ndarray, new_type: Union[str, int]) -> np.ndarray:
     """
     img = img.astype(np.uint8)
     # Not getting into that debate
-    if type(new_type) is str:
-        if new_type.upper() in ["GRAY", "GREY"]:
-            new_type = 1
+    if type(new_color) is str:
+        if new_color.upper() in ["GRAY", "GREY"]:
+            new_color = 1
         # Also not getting into that debate
-        elif new_type.upper() in ["BGR", "COLOR", "COLOUR"]:
-            new_type = 3
-        elif new_type.upper() in ["BGRA", "TRANSPARENT"]:
-            new_type = 4
+        elif new_color.upper() in ["BGR", "COLOR", "COLOUR"]:
+            new_color = 3
+        elif new_color.upper() in ["BGRA", "TRANSPARENT"]:
+            new_color = 4
         else:
-            new_type = -1
+            new_color = -1
 
-    if new_type not in [1, 3, 4]:
+    if new_color not in [1, 3, 4]:
         raise ValueError(
             "The image type {} is not supported. Currently supported: GRAY (1), BGR (3) and BGRA (4)".format(
-                new_type
+                new_color
             )
         )
 
@@ -64,20 +64,20 @@ def convert_color(img: np.ndarray, new_type: Union[str, int]) -> np.ndarray:
         )
 
     # That was easy
-    if currentType == new_type:
+    if currentType == new_color:
         return img
 
-    if currentType == 1 and new_type == 3:
+    if currentType == 1 and new_color == 3:
         return cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-    if currentType == 1 and new_type == 4:
+    if currentType == 1 and new_color == 4:
         return cv2.cvtColor(img, cv2.COLOR_GRAY2BGRA)
 
-    if currentType == 3 and new_type == 1:
+    if currentType == 3 and new_color == 1:
         return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    if currentType == 3 and new_type == 4:
+    if currentType == 3 and new_color == 4:
         return cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
 
-    if currentType == 4 and new_type == 1:
+    if currentType == 4 and new_color == 1:
         return cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
-    if currentType == 4 and new_type == 3:
+    if currentType == 4 and new_color == 3:
         return cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
