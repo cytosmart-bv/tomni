192073134170f856d7b4fea9a9c15270c26d2af9
LAPTOP-KV55JQ73\janni <janniklas.schneider@cytosmart.com>
2022-12-14 15:07:55 +0100

    Add unit test.

---
+++ tomni/cytosmart_data_format/utils/test_utils.py
@@ -0,0 +1,121 @@
+from unittest import TestCase
+
+import numpy as np
+
+from tomni.cytosmart_data_format.annotations.point.main import Point
+from tomni.cytosmart_data_format.annotations.polygon.main import Polygon
+
+from .main import parse_points_to_contour, rdp_compression, recursive_compression
+
+
+class TestUtils(TestCase):
+    def setUp(self) -> None:
+        self.epsilon = 0.9
+        self.n_iter = 1
+        self.circular_points = [
+            Point(1, 2),
+            Point(1, 4),
+            Point(2, 5),
+            Point(4, 5),
+            Point(5, 4),
+            Point(5, 2),
+            Point(4, 1),
+            Point(3, 1),
+            Point(2, 1),
+        ]
+        self.star_shaped_points = [
+            Point(1, 3),
+            Point(2, 3),
+            Point(3, 5),
+            Point(5, 3),
+            Point(3, 1),
+            Point(2, 2),
+        ]
+        self.rectangle_points = [
+            Point(1, 5),
+            Point(3, 5),
+            Point(5, 5),
+            Point(5, 1),
+            Point(3, 1),
+            Point(1, 1),
+        ]
+        self.triangle_points = [
+            Point(1, 5),
+            Point(3, 1),
+            Point(5, 5),
+        ]
+
+    def tearDown(self):
+        pass
+
+    def test_rdp_compression_star(self):
+        expected = [
+            Point(x=1, y=3),
+            Point(x=3, y=5),
+            Point(x=5, y=3),
+            Point(x=3, y=1),
+            Point(x=2, y=2),
+        ]
+        actual = rdp_compression(self.star_shaped_points, self.epsilon)
+
+        self.assertEqual(expected, actual)
+
+    def test_rdp_compression_circular(self):
+        expected = [
+            Point(x=1, y=2),
+            Point(x=1, y=4),
+            Point(x=4, y=5),
+            Point(x=5, y=2),
+            Point(x=2, y=1),
+        ]
+        actual = rdp_compression(self.circular_points, self.epsilon)
+
+        self.assertEqual(expected, actual)
+
+    def test_rdp_compression_triangle(self):
+        expected = [Point(x=1, y=5), Point(x=3, y=1), Point(x=5, y=5)]
+
+        actual = rdp_compression(self.triangle_points, self.epsilon)
+
+        self.assertEqual(expected, actual)
+
+    def test_rdp_compression_rectangle(self):
+        expected = [Point(x=1, y=5), Point(x=5, y=5), Point(x=5, y=1), Point(x=1, y=1)]
+
+        actual = rdp_compression(self.rectangle_points, self.epsilon)
+
+        self.assertEqual(expected, actual)
+
+    def test_recursive_compression_triangle(self):
+        expected = [Point(x=1, y=5), Point(x=5, y=5)]
+
+        actual = recursive_compression(self.triangle_points, self.n_iter)
+
+        self.assertEqual(expected, actual)
+
+    def test_recursive_compression_rectangle(self):
+        expected = [Point(x=1, y=5), Point(x=5, y=5), Point(x=3, y=1)]
+
+        actual = recursive_compression(self.rectangle_points, self.n_iter)
+
+        self.assertEqual(expected, actual)
+
+    def test_recursive_compression_star(self):
+        expected = [Point(x=1, y=3), Point(x=3, y=5), Point(x=3, y=1)]
+
+        actual = recursive_compression(self.star_shaped_points, self.n_iter)
+
+        self.assertEqual(expected, actual)
+
+    def test_recursive_compression_circular(self):
+        expected = [
+            Point(x=1, y=2),
+            Point(x=2, y=5),
+            Point(x=5, y=4),
+            Point(x=4, y=1),
+            Point(x=2, y=1),
+        ]
+
+        actual = recursive_compression(self.circular_points, self.n_iter)
+
+        self.assertEqual(expected, actual)
