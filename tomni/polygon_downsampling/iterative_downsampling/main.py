09d015db93fbd5802dda7ad693303fc6b72ca6ce
jellevkerk <jelle.95@live.nl>
2023-05-10 15:19:25 +0200

    n_iter can be 0 returns same

--- tomni/polygon_downsampling/iterative_downsampling/main.py
+++ tomni/polygon_downsampling/iterative_downsampling/main.py
@@ -11,7 +11,7 @@ def iterative_downsampling(points: list, n_iter: int) -> list:
         A list of 2D points, each represented as dict with {"x": 1, "y": 0}. The number of points in the output
         list is half the number of points in the input list, repeated `n_iter` times.
     """
-    assert n_iter > 0, "Nr of iterations must be positive."
+    assert n_iter >= 0, "Nr of iterations must be zero or positive."
 
     for _ in range(n_iter):
         points = points[::2]
