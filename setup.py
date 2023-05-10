014b4a2af7d48be6516d0f91362ea3f3c30e3b9b
jellevkerk <jelle.95@live.nl>
2023-05-10 15:23:06 +0200

    1.16.0 -> 1.16.1

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/cytosmart-bv/tomni",
-    version="1.16.0",
+    version="1.16.1",
     zip_safe=False,
 )
